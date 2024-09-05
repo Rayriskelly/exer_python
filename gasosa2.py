7af9c8bb43497b64f29d344372da01f4883f55df
Rayris Kelly <101639955+Rayriskelly@users.noreply.github.com>
2024-09-05 08:18:46 -0300

    exercicio calculo com valor da gasolina
    gasosa2.py mostra o processo aceitando (G, E, g, e)
    
    ecerciciogasosa.py aceita apenas os comandos com (G e E)

---
+++ gasosa2.py
@@ -0,0 +1,21 @@
+G = 5.80
+E = 4.90
+
+combustivel = input("Qual tipo de combustivel utilizado? G- gasolina E- etanol")
+if combustivel != 'E' and combustivel != 'e' and combustivel != 'G' and combustivel != 'g':
+    print("Combustível inválido!")
+
+else:
+    print("Combustível válido")
+    if combustivel == 'G':
+        litros = float(input("Quantos litros foram comprados?"))
+
+        valortotal = litros * 5.80
+        print(f"O valor a ser pago é R${valortotal}")
+
+    else:
+        combustivel == 'E'
+        litros = float(input("Quantos litros foram comprados?"))
+
+        valortotal = litros * 4.90
+        print(f"O valor a ser pago é R${valortotal}")
