2c885b41c86e7b1f9eab90f520fc683585743274
Rayris Kelly <101639955+Rayriskelly@users.noreply.github.com>
2024-09-10 09:11:19 -0300

    indicar o mês de acordo com o número inserido pelo usuário, se o número for <1 ou >12 o resultado deve receber a mensagem (Mês inexistente/inválido)

---
+++ meses.py
@@ -0,0 +1,43 @@
+numeromes = int(input("Digite o número do mês desejado: "))
+
+
+
+if numeromes  == 1:
+    print("Janeiro")
+elif numeromes == 2:
+    print("Fevereiro")
+
+elif numeromes == 3:
+    print("Março")
+
+elif numeromes == 4:
+    print("Abril")
+
+elif numeromes == 5:
+    print("Maio")
+
+elif numeromes == 6:
+    print("Junho")
+
+elif numeromes == 7:
+    print("Julho")
+
+elif numeromes == 8:
+    print("Agosto")
+
+elif numeromes == 9:
+    print("Setembro")
+
+elif numeromes == 10:
+    print("Outubro")
+
+elif numeromes == 11:
+    print("Novembro")
+
+elif numeromes == 12:
+    print("Dezembro")
+
+
+else:
+    numeromes < 1 or numeromes > 12
+    print("Mês inexistente")
